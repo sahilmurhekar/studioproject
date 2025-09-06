9d61760de8eafd2c97dbb582f6b2df680bc26505
Sahil Murhekar <sahil.murhekar2004@gmail.com>
2025-09-06 09:42:41 +0530

    Bug fixing

--- frontend/src/components/LightRays.jsx
+++ frontend/src/components/LightRays.jsx
@@ -2,7 +2,7 @@ import { useRef, useEffect, useState } from "react";
 import { Renderer, Program, Triangle, Mesh } from "ogl";
 import "../App.css";
 
-const DEFAULT_COLOR = "#990099";
+const DEFAULT_COLOR = "#DB3BFF";
 
 const hexToRgb = (hex) => {
   const m = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
